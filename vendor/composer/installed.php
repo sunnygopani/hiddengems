0b87b496fec374f6e3096f86531035efb8d7987c
Sunny Gopani <sunny.gopani@gmail.com>
2021-09-14 12:03:44 -0700

    update back composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '44492f6efc9915872c6bcbd8b5aed153b262d5a7',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '44492f6efc9915872c6bcbd8b5aed153b262d5a7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
