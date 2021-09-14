6f4f5db1de17c445d2a5b54a28d4b49da24d3341
Sunny Gopani <sunny.gopani@gmail.com>
2021-09-14 00:59:16 -0700

    Add the php page for it

---
+++ my_business.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<title>Hidden Gems</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+body { margin: 0px }
+.hg-bar-background {
+	color: #000!important;
+    background-color: #f1f1f1!important;
+}
+.hg-bar {
+	width: 100%;
+    overflow: hidden;
+	font-size: 24px!important;
+	border-bottom: 1px solid #ccc!important;
+	color: #000!important;
+    background-color: #fff!important;
+	box-sizing: inherit;
+	content: "";
+    display: table;
+    clear: both;
+}
+.hg-bar .hg-bar-item {
+	padding: 8px 16px;
+	float: left;
+    width: auto;
+    border: none;
+    display: block;
+    outline: 0;
+}
+.hg-red, .hg-hover-red:hover {
+    color: #fff!important;
+    background-color: #f44336!important;
+}
+.hg-bar .hg-button {
+    white-space: normal;
+}
+.hg-text-red {
+    color: #f44336!important;
+}
+.hg-text-grey {
+    color: #757575!important;
+}
+.hg-margin-right {
+    margin-right: 16px!important;
+}
+.hg-right {
+    float: right!important;
+}
+.logo {
+	height: 55px;
+    vertical-align: middle;
+    display: table-cell;
+}
+.lwidth{
+	max-width: 126px !important;
+}
+.hg-image {
+    max-width: 100%;
+    height: auto;
+}
+img {
+    vertical-align: middle;
+	border-style: none;
+}
+img[Attributes Style] {
+    width: 1500px;
+    aspect-ratio: auto 1500 / 700;
+    height: 700px;
+}
+.hg-margin-top {
+    margin-top: 16px!important;
+}
+.hg-container {
+    padding: 0.01em 16px;
+}
+h3 {
+    display: block;
+    font-size: 1.17em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+}
+h6 {
+    font-size: 16px;
+}
+.hg-footer{
+	margin-bottom: 16px!important;
+	text-align: center!important;
+	opacity: 0.60;
+	padding: 0.01em 16px;
+	display: block;
+}
+</style>
+<body class="hg-bar-background">
+
+<!-- Navigation Bar -->
+<div class = "hg-bar">
+  <a href="#" class="hg-bar-item hg-button hg-text-red hg-hover-red logo hg-margin-right"><img alt="" src="hiddengems_logo.png" class="lwidth"></a>
+  <a href="#" class="hg-bar-item hg-button hg-right hg-hover-red hg-text-grey"><i class="fa fa-search lwidth"></i></a>
+</div>
+<img class="w3-image" src="homepage.jpg" alt="London" width="1500" height="700">
+<div class="hg-container hg-margin-top">
+    <h3>So who are we?</h3>
+    <h6><p>We are a team of people who are passionate about travel and exploring unconventional places that we like to call hidden gems around the world. If you often look for unconventional tourist spots while planning your travel, this is the right place for you. Our sole purpose is to encourage and indulge your curiosity about the world and help create a unique experience for your journey.</p></h6>
+	<h3>What do we do?</h3>
+	<h6><p>We offer integrated travel solutions dealing in itinerary planning, hotel booking, air tickets, and travel insurance. In addition, we also offer tailor made travel packages based on the time and preferences that best suits your needs. Hidden Gems provides great offers, exclusive discounts, some of the lowest airfares, and unpararreled service with a renlentless focus to delight the travellers.</p></h6>
+	<h3>Journey so far!</h3>
+	<h6><p>Hidden Gems was found by two friends on their solo trips who met at Horeshoe Bend, Utah, in 2015. They spent a week exploring places that were unconventional and ended up creating some amazing unique experiences. They decided to share their experiences and created a business together based on the exciting idea of curating travel in a completely different way from what was then on the market. The two went on to gain valuable experience in the corporate world, but that big idea refused to go away. Whenever they met, they fuelled each other’s excitement. They looked long and hard at the lifestyles of people around them: busy working lives, limited time or insight to research and plan a complicated travel experience, yet a desire to make the most of every minute away. Over the years, they have had 858 successful tours, and 2500 happy travellers.</p></h6>
+	<h3>Our Mission</h3>
+	<h6><p>Encourage and indulge people's curiosity about the world and help them create a unique experience in their journey</p></h6>
+</div>
+</body>
+<footer class="hg-footer">
+  <p>© 2021 Hidden Gems</p>
+</footer>
+</html>
